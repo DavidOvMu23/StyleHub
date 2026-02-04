8a66c42c53c1b8878839694b8548c12415ff2bc2
DavidOvMu23 <david23oviedo@gmail.com>
2026-02-04 14:35:09 +0100

    crear la vista de estilista que se me había olvidado (ya tenia hecho el model de antes) y he hecho la vista kanban

--- volumesOdoo/addons/stylehub/__manifest__.py
+++ volumesOdoo/addons/stylehub/__manifest__.py
@@ -5,6 +5,7 @@
     'data': [
         'security/ir.model.access.csv', # Permisos de acceso a los modelos
         'views/servicio_views.xml',
+        'views/estilista_views.xml',
         'views/cita_views.xml',
     ],
     'installable': True,
